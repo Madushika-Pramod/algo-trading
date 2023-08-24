77f3eba7c3cde269a0708316d925d4d9a84a5c10
madushika <madushika4@gmail.com>
2023-08-24 17:56:24 +0530

    add adoptive_strategy.py

---
+++ src/broker/src/alpaca_trader.py
@@ -0,0 +1,217 @@
+import asyncio
+import json
+import os
+
+import websockets
+from alpaca.data.historical.stock import StockHistoricalDataClient
+from alpaca.data.requests import StockLatestTradeRequest
+from alpaca.trading import OrderSide, TimeInForce
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import MarketOrderRequest, TrailingStopOrderRequest, LimitOrderRequest, StopOrderRequest
+
+from app.src import constants
+from app.src.voice_alert import voice_alert
+
+
+def get_trade_updates():
+    # Create a new loop for the current thread
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        print('Receiving alpaca trading updates')
+        # Now use this loop to run your async function
+        loop.run_until_complete(alpaca_trade_updates_ws())
+    finally:
+        print("trading updates stopped")
+        loop.close()
+
+
+async def alpaca_trade_updates_ws():
+    async with websockets.connect(constants.trade_stream_wss) as ws:
+        # Authenticate
+        auth_data = {
+            "action": "auth",
+            "key": os.environ.get("API_KEY"),
+            "secret": os.environ.get("SECRET_KEY")
+        }
+        await ws.send(json.dumps(auth_data))
+
+        # Listen to trade updates
+        listen_data = {
+            "action": "listen",
+            "data": {
+                "streams": ["trade_updates"]
+            }
+        }
+        await ws.send(json.dumps(listen_data))
+
+        for _ in range(2):
+            message = await ws.recv()
+            print(f'trade update: {message}')
+
+        # Receive messages
+        while True:
+            message = await ws.recv()
+            print(f'trade update: {message}')
+            trade = json.loads(message)['data']
+            if trade['event'] == 'new' or trade['event'] == 'accepted':
+                constants.pending_order = trade['order']  # todo
+                voice_alert(f"say a {trade['order']['side']} order is placed", 1)
+                voice_alert("say placed", 1)
+                print(
+                    f"a {trade['order']['side']} order is placed at price {trade['order']['hwm']} with {trade['order']['qty']} of quantity\nOrder id={trade['order']['id']}")
+
+            elif trade['event'] == 'filled':
+                constants.accepted_order = trade['order']
+                voice_alert(f"say a {trade['order']['side']} order is executed", 1)
+                voice_alert("say executed", 1)
+                print(
+                    f"a {trade['order']['side']} order is executed at price {trade['order']['stop_price']} with {trade['order']['qty']} of quantity\nOrder id={trade['order']['id']}")
+
+            elif trade['event'] == 'canceled':
+
+                voice_alert(f"say a {trade['order']['side']} order is canceled", 1)
+                voice_alert("say canceled", 1)
+                print(
+                    f"a {trade['order']['side']} order is canceled at price {trade['order']['stop_price']} with {trade['order']['qty']} of quantity\nOrder id={trade['order']['id']}")
+
+            # q.put(message)
+
+
+# asyncio.get_event_loop().run_until_complete(alpaca_ws())
+
+
+def get_last_trade_from_sdk():
+    client = StockHistoricalDataClient(api_key=os.environ.get("API_KEY"), secret_key=os.environ.get("SECRET_KEY"))
+    request = StockLatestTradeRequest(symbol_or_symbols=constants.symbol)
+    return client.get_stock_latest_trade(request)
+
+
+class AlpacaTrader:
+    def __init__(self):
+        self.algo_price = None
+        self.trading_client = TradingClient(os.environ.get("API_KEY"), os.environ.get("SECRET_KEY"), paper=True)
+        self.account = self.trading_client.get_account()
+        self.cash = self.account.buying_power  # buying_power todo
+
+    def buy(self, price):
+        self.account = self.trading_client.get_account()
+        self.algo_price = price
+        trailing_stop_order_data = TrailingStopOrderRequest(
+            symbol=constants.symbol,
+            side=OrderSide.BUY,
+            time_in_force=TimeInForce.DAY,
+            trail_percent=0.1,
+        )
+
+        current_price = get_last_trade_from_sdk()[constants.symbol].price
+
+        if current_price < self.algo_price:  # or self.account.daytrade_count == 3:  # price decreasing or day trade count reach
+            print(f"algo price={self.algo_price} > current price={current_price} <=> price decreasing")
+            return ""
+
+        # execute new trailing stop order
+        trailing_stop_order_data.qty = self._buy_quantity(current_price)
+        if trailing_stop_order_data.qty > 0:
+            order = self.trading_client.submit_order(order_data=trailing_stop_order_data).id
+            _ = self.market_buy(
+                notional=float(self.account.buying_power) - current_price * trailing_stop_order_data.qty).id
+            return order
+
+        return ""
+
+    # def market_order(self, price):
+    #     #todo get pre-market bar data for improve this
+    #
+    #
+    #     trailing_stop_order_data = Stop(
+    #         symbol=constants.symbol,
+    #         qty=self.trading_client.get_open_position(constants.symbol).qty,
+    #         side=OrderSide.SELL,
+    #         time_in_force=TimeInForce.DAY,
+    #         trail_percent=0.1,
+    #     )
+    #
+    #
+    #     # execute new trailing stop order
+    #     if trailing_stop_order_data.qty > 0:
+    #         return self.trading_client.submit_order(order_data=trailing_stop_order_data).id
+    #     return ""
+
+    def market_buy(self, notional=0.0, extended_hours=False):
+
+        market_order_data = MarketOrderRequest(
+            symbol=constants.symbol,
+            side=OrderSide.BUY,
+            time_in_force=TimeInForce.DAY,
+            notional=notional,
+            extended_hours=extended_hours
+
+        )
+        return self.trading_client.submit_order(order_data=market_order_data)
+
+    def market_sell(self, notional=0.0, extended_hours=False):
+        market_order_data = MarketOrderRequest(
+            symbol=constants.symbol,
+            side=OrderSide.SELL,
+            time_in_force=TimeInForce.DAY,
+            notional=notional,
+            extended_hours=extended_hours
+        )
+        return self.trading_client.submit_order(order_data=market_order_data)
+
+    def sell(self, price):
+        self.account = self.trading_client.get_account()
+        self.algo_price = price
+        current_price = get_last_trade_from_sdk()[constants.symbol].price
+
+        trailing_stop_order_data = TrailingStopOrderRequest(
+            symbol=constants.symbol,
+            qty=self.trading_client.get_open_position(constants.symbol).qty,
+            side=OrderSide.SELL,
+            time_in_force=TimeInForce.DAY,
+            trail_percent=0.1,
+        )
+        if current_price > self.algo_price:  # price increasing
+            print(f"algo price={self.algo_price} < current price={current_price} <=> price increasing")
+            return ""
+        # execute new trailing stop order
+        if trailing_stop_order_data.qty > 0:
+            order = self.trading_client.submit_order(order_data=trailing_stop_order_data).id
+            _ = self.market_sell(
+                notional=float(self.account.buying_power) - current_price * trailing_stop_order_data.qty).id
+            return order
+        return ""
+
+    def _buy_quantity(self, price):
+
+        # Calculate maximum shares factoring in the commission
+        return int(float(self.account.buying_power) / (price + constants.commission))
+        # todo check this, when calling saved variable account.cash, all ways get new cash value
+
+
+# t = AlpacaTrader()
+# #
+# stop_order_data = StopOrderRequest(
+#     symbol=constants.symbol,
+#     side=OrderSide.BUY,
+#     time_in_force=TimeInForce.DAY,
+#     qty=1,
+#     stop_price=100,
+#     extended_hours=True
+# )
+#
+# limit_order_data = LimitOrderRequest(
+#     symbol=constants.symbol,
+#     side=OrderSide.BUY,
+#     time_in_force=TimeInForce.DAY,
+#     qty=1,
+#     limit_price=200,
+#     extended_hours=True
+# )
+# t.trading_client.submit_order(order_data=limit_order_data)
+#
+
+# get_trade_updates()
+
+
---
+++ src/broker/src/trading_view_stream.py
@@ -0,0 +1,163 @@
+import json
+import queue
+import re
+import ssl
+import threading
+
+import pandas as pd
+import websocket
+
+
+def extract_data(data):
+    data_points = re.split(r'~m~\d+~m~', data)[1:]
+    results = []
+    pattern = r'((?<=NASDAQ:)\w+).*v":({.+\d})'
+
+    for point in data_points:
+
+        match = re.search(pattern, point)
+        if match:
+            symbol = match.group(1)
+            json_ob = json.loads(match.group(2))
+
+            results.append({
+                "symbol": symbol,
+                "volume": json_ob.get('volume'),
+                "last_price": json_ob.get('lp'),
+                "time": json_ob.get('lp_time'),
+                "p_change": json_ob.get('ch'),
+                "ch_percentage": json_ob.get('chp'),
+                "extended_hours_price": json_ob.get('rtc'),
+                "ehp_percentage": json_ob.get('rchp'),
+
+            })
+
+    # print(results)
+    return results
+
+
+def write_to_csv(df4, filename='trading_view_output5.csv'):
+    # Write the DataFrame to a CSV file
+    df4.to_csv(filename, index=False)
+
+    print(f"Data written to {filename}")
+
+
+def build_dataframe(lists):
+    # Flatten the list of lists into a single list of dictionaries
+    flattened_data = [item for sublist in lists for item in sublist]
+
+    # Convert this flattened list into a pandas DataFrame
+    df3 = pd.DataFrame(flattened_data,
+                       columns=['symbol', 'volume', 'last_price', 'time', 'p_change', 'ch_percentage',
+                                "extended_hours_price", "ehp_percentage"])
+
+    # columns = ['symbol', 'volume', 'last_price', 'datetime', 'cumulative_change', 'cc_percentage',
+    #            "extended_hours_price", "ehp_change"])
+
+    # df3['last_price'].fillna(method='ffill', inplace=True)
+    # df3['p_change'].fillna(method='ffill', inplace=True)
+    # df3['ch_percentage'].fillna(method='ffill', inplace=True)
+    # df3['symbol'].fillna(method='ffill', inplace=True)
+
+    # Convert the 'datetime' column to integers
+    # Attempt to convert the 'datetime' column to integers, setting errors='coerce' to turn failures into NaN
+    df3['time'] = pd.to_numeric(df3['time'], errors='coerce').astype('Int64')
+
+    # Calculate the forward and backward fill
+    ffill = df3['time'].fillna(method='ffill')
+    bfill = df3['time'].fillna(method='bfill')
+
+    # Calculate the midpoint
+    midpoint = (ffill + bfill) // 2  # Use // for integer division
+
+    # Where the original 'time' column is NaN, replace with the midpoint
+    df3['time'] = df3['time'].where(df3['time'].notna(), midpoint)
+    df3['datetime'] = pd.to_datetime(df3['time'], unit='s')
+
+    return df3
+
+
+class TradingViewWebSocket:
+    def __init__(self, data_queue):
+        self.thread = None
+        self.ws = None
+        self.headers = {
+            'Connection': 'Upgrade',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+            'Upgrade': 'websocket',
+            'Sec-WebSocket-Version': '13',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Host': 'data.tradingview.com',
+        }
+        self.data_lists = []
+        self.data_queue = data_queue
+
+    def on_open(self, ws):
+        print("Websocket opened")
+        # ws.send('~m~36~m~{"m":"set_data_quality","p":["low"]}')
+        ws.send('~m~52~m~{"m":"quote_create_session","p":["qs_2YiWuxHOASlh"]}')
+        ws.send(
+            '~m~474~m~{"m":"quote_set_fields","p":["qs_2YiWuxHOASlh","base-currency-logoid","ch","chp","currency-logoid","currency_code","currency_id","base_currency_id","current_session","description","exchange","format","fractional","is_tradable","language","local_description","listed_exchange","logoid","lp","lp_time","minmov","minmove2","original_name","pricescale","pro_name","short_name","type","typespecs","update_mode","volume","value_unit_id","rchp","rtc","country_code","provider_id"]}')
+        ws.send('~m~64~m~{"m":"quote_add_symbols","p":["qs_2YiWuxHOASlh","NASDAQ:GOOGL"]}')
+
+    def on_message(self, ws: websocket, message):
+        pattern = r"^~m~\d+~m~~h~\d+"
+        if re.match(pattern, message):
+            ws.send(message)
+
+        else:
+            d = extract_data(message)
+            if len(d) > 0:
+                self.data_lists.append(d)
+                self.data_queue.put(d)
+                print(d)
+
+    def on_error(self, error):
+        print(f"Process terminated: {error}")
+
+    def on_close(self, ws, status_code, msg):
+
+        df2 = build_dataframe(self.data_lists)
+        write_to_csv(df2)
+        self.data_lists.clear()
+        print(f"Process closed: {status_code}:{msg}")
+
+    def stop(self):
+        df2 = build_dataframe(self.data_lists)
+        write_to_csv(df2)
+        self.data_lists.clear()
+        if self.ws:
+            self.ws.close()
+            self.thread.join()
+
+    def start(self):
+        def run_ws():
+            self.ws = websocket.WebSocketApp(
+                'wss://data.tradingview.com/socket.io/websocket',
+                header=self.headers,
+                on_open=self.on_open,
+                on_message=self.on_message,
+                on_close=self.on_close,
+                on_error=self.on_error)
+
+            self.ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+
+        self.thread = threading.Thread(target=run_ws, daemon=True)
+        self.thread.start()
+
+
+# # Example of how to use the class
+# if __name__ == "__main__":
+# tv_ws = TradingViewWebSocket(queue.Queue)
+# tv_ws.start()
+# try:
+#     tv_ws.thread.join()
+# except KeyboardInterrupt:
+#     print("Main loop interrupted. Cleaning up...")
+#     tv_ws.stop()
+
+# disable queue put
