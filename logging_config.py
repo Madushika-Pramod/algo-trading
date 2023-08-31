34c3930662319cc84656212872d2a082f8f9fd35
madushika <madushika4@gmail.com>
2023-08-31 15:38:34 +0530

    update trading_view_stream.py

--- logging_config.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-
-def setup_logging():
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    # You can also add more advanced configurations here,
-    # like setting up loggers, handlers, and formatters.
