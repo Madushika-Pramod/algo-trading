b18728454c602fb339c95d9c8c308b95e453efcb
madushika <madushika4@gmail.com>
2023-09-28 13:10:37 +0530

    update crypto_data.py

--- src/broker/src/crypto_data.py
+++ src/broker/src/crypto_data.py
@@ -1,9 +1,16 @@
 import csv
+import json
 import logging
 import os
+import queue
+import threading
+
+import backtrader as bt
+
 from datetime import datetime, timedelta
 
 from alpaca.data import CryptoHistoricalDataClient, CryptoBarsRequest, TimeFrame
+from websocket import WebSocketApp
 
 from app.src import constants
 
@@ -47,5 +54,125 @@ class AlpacaCryptoHistoricalData:
 
         logging.info("Data has been written to '{}' file.".format(self.csv_file_path))
 
+class Alpaca_Crypto_WebSocket:
 
-AlpacaCryptoHistoricalData("LINK/USD", 1, constants.crypto_file_path).save_to_csv()
+    def __init__(self, key, secret, url, data_queue):
+        self.ws = None
+        self.thread = None
+        self.key = key
+        self.secret = secret
+        self.url = url
+        self.data_queue = data_queue
+
+    def on_message(self, ws, message):
+        for d in json.loads(message):
+        # print(f'data update:{d}')
+            if d.get('T') == 't':
+                self.data_queue.put(d)
+
+    def on_error(self, ws, error):
+        logging.critical(f"Error occurred on live data stream: {error}")
+        # news(f"Error occurred on alpaca live data stream: {error}")
+
+    def on_close(self, ws, close_status_code, close_msg):
+        logging.warning("live data stream closed")
+        # news("live data stream closed")
+
+    def on_open(self, ws):
+        logging.info("live crypto data stream opened")
+        auth_data = {
+            "action": "auth",
+            "key": self.key,
+            "secret": self.secret
+        }
+        ws.send(json.dumps(auth_data))
+
+        listen_message = {
+            "action": "subscribe",
+            "trades": ["BTC/USD"],
+            # "bars": ["BTC/USD"]
+        }
+        ws.send(json.dumps(listen_message))
+
+    def start(self):
+        def run_ws():
+            self.ws = WebSocketApp(self.url,
+                                   on_message=self.on_message,
+                                   on_error=self.on_error,
+                                   on_close=self.on_close,
+                                   on_open=self.on_open)
+
+            self.ws.run_forever()
+
+        self.thread = threading.Thread(target=run_ws)
+        self.thread.start()
+
+    def stop(self):
+        if self.ws:
+            self.ws.close()
+            self.thread.join()
+# class AlpacaStreamData(bt.feed.DataBase):
+#
+#     def __init__(self, q=queue.Queue()):
+#         # super().__init__()
+#         self.ws = None
+#         self.data_queue = q
+#
+#     def start(self):
+#         self.ws = AlpacaWebSocket(os.environ.get("API_KEY"), os.environ.get("SECRET_KEY"),
+#                                   constants.data_stream_wss, self.data_queue)
+#         self.ws.start()
+#
+#     def stop(self):
+#         self.ws.stop()
+#
+#     def _load(self):
+#         try:
+#             al_data = self.data_queue.get(timeout=90)  # get_nowait()
+#             # print("alpaca data =", al_data)
+#             self._map_bar(al_data)
+#         except queue.Empty:
+#
+#             logging.warning("queue empty")
+#             return False
+#         except KeyboardInterrupt:
+#             logging.warning("KeyboardInterrupt-the trading has been terminated")
+#             # voice_alert('say the trading has been terminated', 1)
+#             return False
+#
+#         except Exception as e:
+#             logging.error(f"An error occurred from AlpacaStreamData: {e}")
+#             return False
+#
+#         return True
+#
+#     def _map_bar(self, data_dict):
+#
+#         date_string = data_dict.get('t')
+#         if date_string and data_dict.get('T') == 't':
+#
+#             # Trim the string to remove nanoseconds and trailing 'Z'
+#             date_string_trimmed = date_string[:23]
+#
+#             # Convert the string to a datetime object
+#             date = datetime.strptime(date_string_trimmed, '%Y-%m-%dT%H:%M:%S.%f')
+#             self.lines.datetime[0] = bt.date2num(date)
+#             self.lines.close[0] = self.lines.low[0] = self.lines.high[0] = self.lines.open[0] = data_dict["p"]
+#             self.lines.volume[0] = data_dict["s"]
+#             # self.tick_data.append(f'{date_string_trimmed}, {data_dict["p"]}, {data_dict["p"]}, {data_dict["p"]}, {data_dict["p"]},{data_dict["p"]}')
+#             # do this before submit date = datetime.strptime(date_string_trimmed, '%Y-%m-%dT%H:%M:%S.%f')
+#         else:
+#             date_string = data_dict.get('timestamp')
+#             if date_string:
+#                 self.lines.datetime[0] = bt.date2num(date_string)
+#                 self.lines.open[0] = data_dict['open']
+#                 self.lines.high[0] = data_dict['high']
+#                 self.lines.low[0] = data_dict['low']
+#                 self.lines.close[0] = data_dict['close']
+#                 self.lines.volume[0] = data_dict['volume']
+
+
+# AlpacaCryptoHistoricalData("LINK/USD", 6, constants.crypto_file_path).save_to_csv()
+ws = Alpaca_Crypto_WebSocket(os.environ.get("API_KEY"), os.environ.get("SECRET_KEY"),
+                                  constants.data_stream_wss, queue.Queue())
+ws.start()
