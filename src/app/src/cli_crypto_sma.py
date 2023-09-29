974ff92030521fbbb13e9fa524f9e4428be6a335
madushika <madushika4@gmail.com>
2023-09-29 14:33:49 +0530

    strategy updated

---
+++ src/app/src/cli_crypto_sma.py
@@ -0,0 +1,102 @@
+# Importing logger_config to set up application-wide logging and exception handling
+# import logger_config
+import asyncio
+import csv
+import logging
+
+from app.src import constants
+from app.src.back_trader import BacktraderStrategy
+from strategies.src.crypto.sma_strategy import SmaStrategy
+
+
+def run_single(live=False):
+    strategy = (
+        SmaStrategy,
+        dict(slow_period=3, fast_period=4, buying_power=800)  # No Trades: 6, ROI: 10.1%
+        # dict(trail_percent_sell=1.2, trail_percent_buy=1.3, period=3, buying_power=800)  #No Trades: 5, ROI: 10.1%
+    )
+
+    # strategy = (TrailingStopStrategy, {})
+    result = BacktraderStrategy(live).add_strategy(strategy).run()
+    logging.info(
+        f"Number of Trades: {result.trading_count}\nReturn on investment: {round(result.average_return_on_investment * 100, 3)}%")
+
+
+def get_sma_cross_strategy_v2_optimum_params(best_roi=0, slow_period=None, fast_period=None,
+                                             pre_count=0):
+    buying_power = 800
+    count = 0
+    f2 = 0
+    s2 = 0
+    roi_count = 0
+    # statistics = [
+    #     ["iteration", "Trading Count", "Roi", "Period", "Trail Percent Sell", "Trail Percent Buy", "Win count",
+    #      "Loss Count"]]
+    try:
+        for f in range(fast_period, 50):
+            if f == fast_period + 20:
+                print(f"Total Count: {count}")
+                print(f"Best ROI: {best_roi * 100}% at count : {roi_count}")
+                # write_csv(statistics)
+
+                raise Exception("=== Parameter Tuning successfully terminated===")
+            for s in range(slow_period, 50):
+                if s > f:
+                    # if count >= pre_count:
+                    result = BacktraderStrategy(live=False).add_strategy((
+                        SmaStrategy, dict(slow_period=s, fast_period=f, buying_power=buying_power))).run()
+                    # statistics.append(
+                    #     [count, result.trading_count, result.average_return_on_investment, p, ts, tb,
+                    #      result.win_count, result.loss_count])
+                    if result.average_return_on_investment > best_roi:
+                        best_roi = result.average_return_on_investment
+                        roi_count = count
+                        f2 = f
+                        s2 = s
+                        print(
+                            f"count : {count}\nBest ROI: {round(best_roi * 100, 3)}%\nFast Period :{f}\n Slow Period : {s}")
+                    print(count)
+                    # print(result.total_return_on_investment)
+                    count += 1
+
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt received. Performing cleanup...save following data if you can't find tuned parameters")
+        print(f"fast period: {f2}-Slow period: {s2}-current Count: {roi_count}-Best ROI: {best_roi * 100}%")
+        # write_csv(statistics)
+
+
+def write_csv(statistics):
+    # header = ["ROI", "Dev Factor", "Rsi period", "Rsi low", "Rsi high", "Bollinger Period", "Gain value"]
+
+    with open(constants.stat_file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        # writer.writerow(header)  # writing the header
+
+        for entry in statistics:
+            writer.writerow(entry)  # writing each entry as a row
+
+
+configurations_for_sma_cross_v2 = [
+    dict(slow_period=2, fast_period=2),
+    dict(slow_period=2, fast_period=20),
+    dict(slow_period=2, fast_period=40)
+    # ... other configurations
+]
+
+
+async def sma_cross_v2_config_process(config):
+    loop = asyncio.get_running_loop()
+    result = await loop.run_in_executor(None, lambda: get_sma_cross_strategy_v2_optimum_params(**config))
+    # handle the result if needed
+    return result
+
+
+
+async def run_parallel(config_process, configurations):
+    tasks = [config_process(config) for config in configurations]
+    await asyncio.gather(*tasks)
+    logging.info('All functions have finished executing')
+
+
+if __name__ == "__main__":
+    asyncio.run(run_parallel(sma_cross_v2_config_process, configurations_for_sma_cross_v2))
---
+++ src/strategies/src/crypto/sma_strategy.py
@@ -0,0 +1,127 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+
+import backtrader as bt
+import pytz
+
+from broker.src.alpaca_crypto_trader import AlpacaCryptoTrader, CryptoDemoTrader
+from strategies.src.indicators.talib_sma import TALibSMA
+
+
+# todo if price is increasing we should stay
+# todo if price is decreasing we should leave
+
+
+class SmaStrategy(bt.Strategy):
+    params = dict(
+
+        slow_period=3,
+        fast_period=4,
+        buying_power=800
+    )
+
+    def __init__(self):
+
+        self.executor = ThreadPoolExecutor()
+
+        self.win_count = 0
+        self.loss_count = 0
+
+        self.price_of_last_purchase = None
+        self.starting_buying_power = self.p.buying_power
+        self.cumulative_profit = 0
+        self.trading_count = 0
+        self.roi = {}
+        self.average_return_on_investment = 0
+        self.order_quantity = None
+        self.price_of_last_sale = None
+        self.trade_active = False
+        self.live = None
+
+        self.slow_kama = TALibSMA(period=self.p.slow_period)
+        self.fast_kama = TALibSMA(period=self.p.fast_period)
+
+        self.trader = AlpacaCryptoTrader() if self.cerebro.params.live else CryptoDemoTrader(self.p.buying_power)
+
+    def buy_crypto(self):
+        self.trade_active = True
+        self.price_of_last_purchase = self.data.close[0]
+        self.order_quantity = self.order_quantity * self.price_of_last_sale / self.data.close[0]
+        self.notify_order(True)
+
+    def sell_crypto(self):
+        self.trade_active = False
+        self.price_of_last_sale = self.data.close[0]
+        self.notify_order(False)
+
+    def _start_trade(self):
+        if self.trade_active:
+            self._start_sell_process()
+        else:
+            self._start_buy_process()
+
+    def current_price_datetime(self):
+        return bt.num2date(self.data.datetime[0]).replace(tzinfo=pytz.utc).astimezone(
+            pytz.timezone('Asia/Kolkata')).strftime('%Y-%m-%d %I:%M:%S %p')
+
+    def _start_sell_process(self):
+        if (self.data.close[0] < self.data.open[0] and self.slow_kama > self.fast_kama) or self.data.close[
+            0] <= 1.0005 * self.price_of_last_purchase:
+            if self.trader.sell():
+                self.sell_crypto()
+
+    def _start_buy_process(self):
+        if self.data.close[0] > self.data.open[0] and self.slow_kama < self.fast_kama:
+            if self.trader.buy():
+                self.buy_crypto()
+
+    async def async_sleep_and_schedule(self, loop, func):
+        await asyncio.sleep(5)  # Non-blocking sleep
+
+        # Scheduling the synchronous function to the executor
+        loop.run_in_executor(self.executor, func)
+
+    def next(self):
+        # if self.order_quantity is None:
+        #     self.order_quantity = self.trader.crypto_buying_power / self.data.close[0]
+        #     self.price_of_last_sale = self.data.close[0]
+        # self._start_trade()
+        print('NEXT')
+
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(self.async_sleep_and_schedule(loop, self.next))
+
+    def stop(self):
+        # print(f'win count: {self.win_count}\nloss count: {self.loss_count} ')
+        if len(self.roi.values()) > 0:
+            self.average_return_on_investment = sum(self.roi.values()) / len(self.roi.values())
+
+    def log(self, txt, dt=None):
+        """ Logging function for the strategy """
+        dt = dt or self.current_price_datetime()
+        # logging.info(f'{dt}, {txt}')
+
+    def notify_order(self, is_buy_order):
+        """Handle the events of executed orders."""
+
+        if is_buy_order:
+            # self.commission_on_last_purchase = order.executed.comm
+            # self.log(f"Commission on Buy: {order.executed.comm}")
+            # self.log('BUY EXECUTED, %.2f' % order.executed.price)  # executing.price for a buy is next bar's open price
+            self.log('BUY EXECUTED, %.2f' % self.price_of_last_purchase)
+        else:
+            if self.price_of_last_sale - self.price_of_last_purchase < 0:
+                self.loss_count += 1
+            elif self.price_of_last_sale - self.price_of_last_purchase > 0:
+                self.win_count += 1
+
+            self.trading_count += 1
+            self.cumulative_profit += (self.price_of_last_sale - self.price_of_last_purchase) * self.order_quantity
+
+            roi = round(self.cumulative_profit / self.starting_buying_power, 3)
+            self.roi[self.current_price_datetime()] = roi
+            # logging.info(f"{roi * 100}%")
+
+            self.log('SELL EXECUTED, %.2f with quantity of %.10f' % (
+                self.price_of_last_sale, self.order_quantity))
